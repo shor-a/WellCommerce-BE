3a02cfdd6762f0c5a34e1a9d2f195b310ea4609f
Fadli Ashari <ashari.attamimi@gmail.com>
2025-12-09 21:09:46 +0700

    Migrate to TypeScript and add core dependencies

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import express from "express"
+import type { Request, Response, NextFunction } from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({ origin: "*" }))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send({ status: "Route not found" })
+  next()
+})
+
+let port = 7770
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
